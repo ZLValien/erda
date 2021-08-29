a21eaaa4e7de4ff3f754b9a0d104e7d01dd68fbb
ZLValien <1006058235@qq.com>
2021-08-30 01:03:30 +0800

    add test-plan-before plugin

---
+++ modules/pipeline/providers/aop/plugins/pipeline/testplan_before/plugin.go
@@ -0,0 +1,141 @@
+// Copyright (c) 2021 Terminus, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testplan_before
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/erda-project/erda-infra/base/servicehub"
+	"github.com/erda-project/erda-proto-go/core/pipeline/cms/pb"
+	"github.com/erda-project/erda/apistructs"
+	"github.com/erda-project/erda/bundle"
+	"github.com/erda-project/erda/modules/pipeline/providers/aop/aoptypes"
+	"github.com/erda-project/erda/modules/pipeline/providers/aop/plugins_manage"
+)
+
+const (
+	actionTypeAutoTestPlan = "auto-test-plan"
+)
+
+var providerInstance provider
+
+type Plugin struct {
+	aoptypes.PipelineBaseTunePoint
+	CmsService pb.CmsServiceServer
+	Bundle     *bundle.Bundle
+}
+
+func (p *Plugin) Name() string {
+	return "testplan_before"
+}
+
+func (p *Plugin) Handle(ctx *aoptypes.TuneContext) error {
+	// source = autotest
+	if ctx.SDK.Pipeline.PipelineSource != apistructs.PipelineSourceAutoTest || ctx.SDK.Pipeline.IsSnippet {
+		return nil
+	}
+
+	// PipelineYmlName is autotest-plan-xxx
+	pipelineNamePre := apistructs.PipelineSourceAutoTestPlan.String() + "-"
+	testPlanIDStr := strings.TrimPrefix(ctx.SDK.Pipeline.PipelineYmlName, pipelineNamePre)
+	testPlanID, err := strconv.ParseUint(testPlanIDStr, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	// get TestPlan
+	testPlan, err := p.Bundle.GetTestPlanV2(testPlanID)
+	if err != nil {
+		return err
+	}
+	// get config from projectID
+	var autoTestGlobalConfigListRequest apistructs.AutoTestGlobalConfigListRequest
+	autoTestGlobalConfigListRequest.ScopeID = strconv.Itoa(int(testPlan.Data.ProjectID))
+
+	autoTestGlobalConfigListRequest.Scope = "project-autotest-testcase"
+	autoTestGlobalConfigListRequest.UserID = ctx.SDK.Pipeline.PipelineExtra.Snapshot.PlatformSecrets["dice.user.id"]
+	configs, err := p.Bundle.ListAutoTestGlobalConfig(autoTestGlobalConfigListRequest)
+	if err != nil {
+		return err
+	}
+
+	meta := make(apistructs.PipelineReportMeta)
+	ccc, err := json.Marshal(configs)
+	for _, v := range configs {
+		if v.Ns == ctx.SDK.Pipeline.PipelineExtra.Extra.ConfigManageNamespaces[0] {
+			meta["data"] = v
+			break
+		}
+	}
+	if _, ok := meta["data"]; !ok {
+		return errors.New("no found Pipeline NS")
+	}
+
+	// result 信息
+	_, err = ctx.SDK.Report.Create(apistructs.PipelineReportCreateRequest{
+		PipelineID: ctx.SDK.Pipeline.ID,
+		Type:       actionTypeAutoTestPlan,
+		Meta:       meta,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func New() *Plugin {
+	var p Plugin
+	p.CmsService = providerInstance.CmsService
+	p.Bundle = bundle.New(bundle.WithDOP())
+	return &p
+}
+
+type config struct {
+	TuneType    aoptypes.TuneType      `file:"tune_type"`
+	TuneTrigger []aoptypes.TuneTrigger `file:"tune_trigger" `
+}
+
+// +provider
+type provider struct {
+	Cfg        *config
+	CmsService pb.CmsServiceServer `autowired:"erda.core.pipeline.cms.CmsService"`
+}
+
+func (p *provider) Init(ctx servicehub.Context) error {
+	providerInstance = *p
+	for _, tuneTrigger := range p.Cfg.TuneTrigger {
+		err := plugins_manage.RegisterTunePointToTuneGroup(p.Cfg.TuneType, tuneTrigger, New())
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	servicehub.Register("erda.core.pipeline.aop.plugins.pipeline.testplan-before", &servicehub.Spec{
+		ConfigFunc: func() interface{} {
+			return &config{}
+		},
+		Creator: func() servicehub.Provider {
+			return &provider{}
+		},
+	})
+}
